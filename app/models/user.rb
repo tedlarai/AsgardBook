a363d1113327847e884cd23a43cf66f68cb1923a
tedlarai <thadeularanja@gmail.com>
2015-09-16 16:51:18 -0300

    correct omniauth bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,8 +51,11 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      names = auth.info.name.split(" ")
+      first_name = names.shift
+      last_name = names.join(" ")
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
     end
   end
 
