cd30b585458ac1d9212006d22a0ddb7a091c4ec9
tedlarai <thadeularanja@gmail.com>
2015-09-23 10:48:35 -0300

    start adjusting to bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    title = 'ABook'
+    unless page_title.empty?
+      title = page_title + ' | ' + title
+    end
+    title
+  end
 end
