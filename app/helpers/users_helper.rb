56e7b87c5a081e22b7961af4431244ba9ffe92af
tedlarai <thadeularanja@gmail.com>
2015-09-30 17:02:40 -0300

    continue to add presentation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
   def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class:"gravatar")
+    # gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    # image_tag(gravatar_url, alt: user.name, class:"gravatar")
+    image_tag("Icon.png", alt: user.name, class:"gravatar", height: "64px")
   end
 end
